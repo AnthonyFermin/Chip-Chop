9210dcfa0eecfef9e785e8cd28fbaec0ea266f3d
AnthonyFermin <AnthonyFerminAFC@gmail.com>
2015-08-13 21:16:06 -0400

    maps

--- app/src/main/java/madelyntav/c4q/nyc/chipchop/MainActivity.java
+++ app/src/main/java/madelyntav/c4q/nyc/chipchop/MainActivity.java
@@ -1,5 +1,7 @@
 package madelyntav.c4q.nyc.chipchop;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
@@ -10,6 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        SearchMapFragment mapFragment = new SearchMapFragment();
+        ft.replace(R.id.container,mapFragment);
+        ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
+        ft.commit();
 
     }
 

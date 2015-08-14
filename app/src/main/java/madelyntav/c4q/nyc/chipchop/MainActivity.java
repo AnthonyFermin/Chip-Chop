830dae385bc286ac461759d34df08090674c8a7f
AnthonyFermin <AnthonyFerminAFC@gmail.com>
2015-08-14 14:19:51 -0400

    added basic map functionality to mapfragment

--- app/src/main/java/madelyntav/c4q/nyc/chipchop/MainActivity.java
+++ app/src/main/java/madelyntav/c4q/nyc/chipchop/MainActivity.java
@@ -14,12 +14,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        FragmentManager fm = getFragmentManager();
-        FragmentTransaction ft = fm.beginTransaction();
-        SearchMapFragment mapFragment = new SearchMapFragment();
-        ft.replace(R.id.container,mapFragment);
-        ft.setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out);
-        ft.commit();
-
     }
 }

941a815366e9ccdaa3dee389c0609db845b891ce
ahhhlvin <alvkuang@gmail.com>
2015-08-15 17:55:30 -0400

    finished layout for seller profile 6pm

--- app/src/main/java/madelyntav/c4q/nyc/chipchop/fragments/Fragment_SellerProfile.java
+++ app/src/main/java/madelyntav/c4q/nyc/chipchop/fragments/Fragment_SellerProfile.java
@@ -1,23 +1,50 @@
 package madelyntav.c4q.nyc.chipchop.fragments;
 
-import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
+import madelyntav.c4q.nyc.chipchop.DBObjects.Item;
 import madelyntav.c4q.nyc.chipchop.R;
+import madelyntav.c4q.nyc.chipchop.adapters.FoodListAdapter;
 
 
 public class Fragment_SellerProfile extends Fragment {
+
+    private ArrayList<Item> foodItems;
+    private RecyclerView itemsList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_seller_profile, container, false);
+        View root = inflater.inflate(R.layout.fragment_seller_profile, container, false);
+
+
+        foodItems = new ArrayList<>();
+        populateItems();
+
+        itemsList = (RecyclerView) root.findViewById(R.id.seller_profile_items_list);
+        itemsList.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        FoodListAdapter foodListAdapter = new FoodListAdapter(getActivity(), foodItems);
+        itemsList.setAdapter(foodListAdapter);
+
+        return root;
+    }
+
+    //test method to populate RecyclerView
+    private void populateItems(){
+        for(int i = 0; i < 10; i++) {
+            foodItems.add(new Item("test", "Something Fancy", "3", "The fanciest homemade meal you've ever had", "http://wisebread.killeracesmedia.netdna-cdn.com/files/fruganomics/imagecache/605x340/blog-images/food-186085296.jpg"));
+        }
     }
 
 

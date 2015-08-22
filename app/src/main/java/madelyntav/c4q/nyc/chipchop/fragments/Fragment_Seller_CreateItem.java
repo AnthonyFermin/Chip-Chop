dd40830ca3a91f3f15fc2b1b8b55ba40ec05977e
ahhhlvin <alvkuang@gmail.com>
2015-08-22 10:57:14 -0400

    add dish photo function implemented

--- app/src/main/java/madelyntav/c4q/nyc/chipchop/fragments/Fragment_Seller_CreateItem.java
+++ app/src/main/java/madelyntav/c4q/nyc/chipchop/fragments/Fragment_Seller_CreateItem.java
@@ -1,11 +1,18 @@
 package madelyntav.c4q.nyc.chipchop.fragments;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 
 import madelyntav.c4q.nyc.chipchop.R;
 
@@ -14,12 +21,28 @@ import madelyntav.c4q.nyc.chipchop.R;
  */
 public class Fragment_Seller_CreateItem extends Fragment {
 
+    public static final int RESULT_OK = -1;
+    private Uri imageFileUri;
+    Intent intent;
+    ImageButton dishPhotoButton;
+    ImageView dishPhoto;
     Button addButton;
+    private String stringVariable = "file:///sdcard/_pictureholder_id.jpg";
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View root = inflater.inflate(R.layout.fragment_seller__create_item, container, false);
+
+        dishPhoto = (ImageView) root.findViewById(R.id.dish_image);
+
+        dishPhotoButton = (ImageButton) root.findViewById(R.id.dish_image);
+        dishPhotoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showListViewDialog();
+            }
+        });
 
         addButton = (Button) root.findViewById(R.id.add_item_button);
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -31,5 +54,56 @@ public class Fragment_Seller_CreateItem extends Fragment {
 
 
         return root;
+    }
+
+
+
+    //This handles the activity for the intent: using the camera and choosing from a gallery.
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
+            imageFileUri = data.getData();
+        }
+
+        if (requestCode == 0 && resultCode == RESULT_OK) {
+            imageFileUri = Uri.parse(stringVariable);
+        }
+
+
+        if (imageFileUri != null) {
+            dishPhoto.setImageURI(imageFileUri);
+        }
+    }
+
+
+
+    //This is for the dialog box: Camera or Gallery
+    private void showListViewDialog() {
+        AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(getActivity());
+        dialogBuilder.setTitle("Please Choose:");
+        final String[] items = {"Camera", "Gallery"};
+        dialogBuilder.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                if (items[which].equalsIgnoreCase("Camera")) {
+                    intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                    intent.putExtra(android.provider.MediaStore.EXTRA_OUTPUT, Uri.parse(stringVariable));
+
+                    if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+                        startActivityForResult(intent, 0);
+                    }
+                }
+
+                if (items[which].equalsIgnoreCase("Gallery")) {
+                    intent = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                    startActivityForResult(intent, 1);
+                }
+            }
+        });
+        AlertDialog alertDialog = dialogBuilder.create();
+        alertDialog.show();
     }
 }

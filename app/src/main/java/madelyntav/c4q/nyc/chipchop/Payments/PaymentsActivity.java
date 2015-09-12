768f42e7b0830a4668c1bd8050ab2b4bd5ae5a8a
MadelynTav <Madelyntav@gmail.com>
2015-09-12 11:00:19 -0400

    fixed small bug in payments

--- app/src/main/java/madelyntav/c4q/nyc/chipchop/Payments/PaymentsActivity.java
+++ app/src/main/java/madelyntav/c4q/nyc/chipchop/Payments/PaymentsActivity.java
@@ -331,10 +331,10 @@ public class PaymentsActivity extends AppCompatActivity implements GoogleApiClie
                             new Handler().post(new Runnable() {
                                 @Override
                                 public void run() {
-                                    Toast.makeText(PaymentsActivity.this, "Order and Payment Confirmed", Toast.LENGTH_SHORT).show();
                                     Intent intent = new Intent(PaymentsActivity.this, BuyActivity.class);
                                     intent.putExtra("To Orders View", true);
                                     startActivity(intent);
+                                    Toast.makeText(PaymentsActivity.this, "Order and Payment Confirmed", Toast.LENGTH_SHORT).show();
                                 }
                             });
                         }

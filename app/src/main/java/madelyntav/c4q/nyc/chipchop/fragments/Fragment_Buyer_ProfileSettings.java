4f44607d4c65f753cc66275c3b58f57cdd74ca1f
MadelynTav <Madelyntav@gmail.com>
2015-09-13 11:11:48 -0400

    added method to covert image for saving in DB

--- app/src/main/java/madelyntav/c4q/nyc/chipchop/fragments/Fragment_Buyer_ProfileSettings.java
+++ app/src/main/java/madelyntav/c4q/nyc/chipchop/fragments/Fragment_Buyer_ProfileSettings.java
@@ -3,10 +3,16 @@ package madelyntav.c4q.nyc.chipchop.fragments;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,9 +20,15 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import madelyntav.c4q.nyc.chipchop.BuyActivity;
 import madelyntav.c4q.nyc.chipchop.DBObjects.DBHelper;
 import madelyntav.c4q.nyc.chipchop.DBObjects.User;
+import madelyntav.c4q.nyc.chipchop.HelperMethods;
 import madelyntav.c4q.nyc.chipchop.R;
 
 /**
@@ -33,10 +45,11 @@ public class Fragment_Buyer_ProfileSettings extends Fragment {
     private Uri imageFileUri;
     Intent intent;
     private String stringVariable = "file:///sdcard/_pictureholder_id.jpg";
-
+    String filePath;
     DBHelper dbHelper;
     BuyActivity activity;
     User user;
+    String imageLink;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -107,15 +120,24 @@ public class Fragment_Buyer_ProfileSettings extends Fragment {
 
         if (requestCode == 1 && resultCode == RESULT_OK && data != null) {
             imageFileUri = data.getData();
+            filePath = imageFileUri.getPath();
+            rescaleImageForDb(filePath);
+
         }
 
         if (requestCode == 0 && resultCode == RESULT_OK) {
             imageFileUri = Uri.parse(stringVariable);
+            filePath = imageFileUri.getPath();
+            rescaleImageForDb(filePath);
+
         }
 
 
         if (imageFileUri != null) {
             profilePhoto.setImageURI(imageFileUri);
+            filePath = imageFileUri.getPath();
+            rescaleImageForDb(filePath);
+
         }
     }
 
@@ -149,7 +171,74 @@ public class Fragment_Buyer_ProfileSettings extends Fragment {
 //        alertDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
         alertDialog.show();
 
+    }
 
+    private void rescaleImageForDb(final String filePath) {
+        new AsyncTask<Void, Void, Bitmap>() {
+            @Override
+            protected Bitmap doInBackground(Void... voids) {
+                Bitmap bitmap = BitmapFactory.decodeFile(filePath);
+                Bitmap rotatedBitmap = rotateBitmap(bitmap,filePath);
+                Bitmap scaledBitmap;
+                if(rotatedBitmap != null) {
+                    scaledBitmap = Bitmap.createScaledBitmap(rotatedBitmap, 500, 333, false);
+                }else{
+                    scaledBitmap = Bitmap.createScaledBitmap(bitmap, 500,333, false);
+                }
+
+
+                File file = new File(filePath);
+                FileOutputStream fOut = null;
+                try {
+                    fOut = new FileOutputStream(file);
+                    scaledBitmap.compress(Bitmap.CompressFormat.PNG, 85, fOut);
+                    fOut.flush();
+                    fOut.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                return scaledBitmap;
+            }
+
+            @Override
+            protected void onPostExecute(Bitmap bitmap) {
+                super.onPostExecute(bitmap);
+                imageLink = HelperMethods.saveImageToEncodedString(filePath);
+                profilePhoto.setImageBitmap(bitmap);
+                Log.d("Item Creation", "ImageLink: " + imageLink);
+            }
+        }.execute();
+    }
+
+    private Bitmap rotateBitmap(Bitmap bitmap, String filePath){
+        Bitmap rotatedBitmap = null;
+
+        try {
+            ExifInterface exif = new ExifInterface(filePath);
+            int orientation = exif.getAttributeInt(ExifInterface.TAG_ORIENTATION,1);
+            Matrix matrix = new Matrix();
+
+            switch(orientation){
+                case 3:
+                    matrix.postRotate(180);
+                    break;
+                case 6:
+                    matrix.postRotate(90);
+                    break;
+                case 8:
+                    matrix.postRotate(270);
+                    break;
+            }
+
+            rotatedBitmap = Bitmap.createBitmap(bitmap, 0, 0, bitmap.getWidth(), bitmap.getHeight(), matrix, true);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return rotatedBitmap;
     }
 
 }

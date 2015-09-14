efeee4db892bba898b94cd522956f7d4046080d8
MadelynTav <Madelyntav@gmail.com>
2015-09-14 00:27:48 -0400

    added profile info for seller from buyer right after sign up

--- app/src/main/java/madelyntav/c4q/nyc/chipchop/SignupActivity2.java
+++ app/src/main/java/madelyntav/c4q/nyc/chipchop/SignupActivity2.java
@@ -4,9 +4,7 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.ColorDrawable;
 import android.net.Uri;
 import android.os.Bundle;
@@ -16,13 +14,10 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.PhoneNumberFormattingTextWatcher;
 import android.util.Log;
-import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
@@ -235,6 +230,7 @@ public class SignupActivity2 extends AppCompatActivity {
                 user.setPhoneNumber(phoneNumber);
 
                 dbHelper.addUserProfileInfoToDB(user);
+                HelperMethods.setUser(user);
 
                 saveToSharedPrefs();
 
